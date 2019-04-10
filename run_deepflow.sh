87258853a31a595ec0ebbb8c6901893348832018
lukas.mosser <lukas.mosser@gmail.com>
2019-04-10 11:42:00 +0100

    updated shell script for local

--- run_deepflow.sh
+++ run_deepflow.sh
@@ -2,7 +2,7 @@ END=100;
 for i in $(seq 0 $END)
 do 
 python run_deepflow.py --working_dir ./ \
---output_dir runs/test_deepflow_mala \
+--output_dir runs/test_local \
 --matlab_dir mrst/mrst-2018a/modules/optimization/examples/model2Dtest \
 --mrst_dir mrst/mrst-2018a \
 --checkpoints_dir checkpoints \
@@ -10,12 +10,11 @@ python run_deepflow.py --working_dir ./ \
 --optimizer adam \
 --beta1 0.9 \
 --beta2 0.999 \
---lr 1e-1 \
+--lr 3e-2 \
 --iterations 500 \
 --weight_decay 0.0 \
+--optimize_flow \
 --optimize_wells \
---early_stopping \
---target_accuracy 1.0 \
 --use_prior_loss \
 --seed "$i"
 done
